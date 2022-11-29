20274da5e044eb0e34d39b1a264258287635a771
Adeyom <adeyomz.a@gmail.com>
2022-11-29 04:08:07 -0600

    Se sube el proyecto

---
+++ apps/app.js
@@ -0,0 +1,24 @@
+function despedir (){
+    alert ("Adios")
+}
+
+function Edad (){
+    alert ("Cual es tu edad")
+}
+
+
+function Nombre (){
+    alert ("Cual es tu nombre?")
+}
+
+function Carrera  (){
+    alert ("Cual es tu carrera?")
+}
+
+console.log("Hola desde la consola")
+console.log("Esto es un: ")
+console.log(50000000000)
+console.log(56548)
+console.log(5454545455)
+console.log(true)
+console.log(false)
---
+++ apps/app1.js
@@ -0,0 +1,41 @@
+
+// let numMult = parseInt(prompt("Digita el numero del cual quieres la tabla"));
+
+
+// function multiplicacion(n1,n2){
+//     for (let index = 0; index < array.length; index++) {
+//         const element = array[index];
+        
+//     }
+
+//     return parseInt(n1) + parseInt(n2);
+// }
+
+// const frutas = ['sandia', 'pera', 'naranja'];
+// frutas.push('hola');
+// frutas.shift();
+// frutas.pop();
+
+
+// console.log(frutas);
+// for on 
+
+
+let compras = [];
+let opcion;
+let objeto;
+
+
+do {
+    objeto = prompt("Escribe un nuevo objeto para agregar: ");
+    compras.push(objeto);
+    // console.log(compras);
+    console.log [2];
+    opcion = confirm("¿Deseas ingresar un producto?");
+} while (opcion == true);
+
+
+console.log("Fin de la lista");
+
+
+
